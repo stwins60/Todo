62ab82d9ed6b825ea46e3b6a7d0b7d150785121c
Idris Fagbemi <idrisniyi94@gmail.com>
2020-08-22 13:06:57 -0500

    added list of files

---
+++ js/services/firebaseStorage.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAoB1dN-whSysND4WJ2puXoEReFLncekJw",
+    authDomain: "todo-b0b72.firebaseapp.com",
+    databaseURL: "https://todo-b0b72.firebaseio.com",
+    projectId: "todo-b0b72",
+    storageBucket: "todo-b0b72.appspot.com",
+    messagingSenderId: "453262151058",
+    appId: "1:453262151058:web:41f22710f0a0fe7841854b",
+    measurementId: "G-MR3Z8DHWTQ"
+  };
